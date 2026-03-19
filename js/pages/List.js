46a0dd4a841d7468c2105e6b1ae0a8d4edf2ddd4
Rocketpowered-tech <chaserob12345@gmail.com>
2026-03-19 15:42:11 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -117,7 +117,7 @@ export default {
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Levels placing above Xmas Challenge require audible click sounds OR a handcam to have your records be accepted.
+                        All submissions must be recorded and have audible click sounds ( no clickbot )
                     </p>
                     <p>
                         Do not use secret routes or bug routes
